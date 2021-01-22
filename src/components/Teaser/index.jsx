87f5633ab32ff62ce05b6cede1f2e149d2515611
DesiLanz <lanzdesiree@gmail.com>
2021-01-22 13:18:49 +0100

    YESS

--- src/components/Teaser/index.jsx
+++ src/components/Teaser/index.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import "./styles.scss";
+// import React, { useState } from "react";
+// import "./styles.scss";
 
-const Teaser = ({ title, subtitle, src, type }) => {
-  return (
-    <div className={`teaser teaser--${type}`}>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <img src={src} alt="Testbild" />
-    </div>
-  );
-};
+// const Teaser = ({ title, subtitle, src, type }) => {
+//   return (
+//     <div className={`teaser teaser--${type}`}>
+//       <h2>{title}</h2>
+//       <h3>{subtitle}</h3>
+//       <img src={src} alt="Testbild" />
+//     </div>
+//   );
+// };
 
-export default Teaser;
+// export default Teaser;
